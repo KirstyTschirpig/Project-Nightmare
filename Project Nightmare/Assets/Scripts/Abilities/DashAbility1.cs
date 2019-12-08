9941d2ef1e22e24973916408c55d0b23d93e252b
Mika Goetze <mika.goetze@mightykingdom.com>
2019-12-08 19:29:56 +1030

    Added ability editor window etc.

---
+++ Project Nightmare/Assets/Scripts/Abilities/DashAbility1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using KinematicCharacterController;
+using UnityEngine;
+
+public class OtherAbility : PlayerAbility
+{
+    [AbilitySetting] public bool RandomBool;
+
+    [AbilitySetting] public float RandomFloat;
+
+    protected override void AbilityActivated(Transform target)
+    {
+        controller.SetVelocityControlOverride(VelocityOverride);
+    }
+
+    protected override void AbilityFinished()
+    {
+        controller.ClearVelocityControlOverride();
+    }
+
+    protected override void AbilityReady()
+    {
+    }
+
+    private Vector3 VelocityOverride(Vector3 forward, Vector3 up, Vector3 currentVel)
+    {
+//        return currentVel.normalized * DashSpeed;
+        return Vector3.zero;
+    }
+
+    public override void Initialise()
+    {
+        base.Initialise();
+        AbilityName = "Dash";
+        AbilityCooldown = 1.0f;
+        AbilityDuration = 0.2f;
+//        DashSpeed = 50;
+    }
+}
---
+++ Project Nightmare/Assets/Scripts/Editor/AbilityEditorWindow.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+public class AbilityEditorWindow : OdinEditorWindow
+{
+    [ValueDropdown("abilityTypes")] public Type targetAbility;
+    private Type lastTargetAbility;
+
+    private Type[] abilityTypes;
+
+    private List<FieldInfo> fieldInfos;
+
+    private object targetAbilityObject;
+
+    private const string PATH_PREFIX = "Assets/Resources/";
+
+    private List<FieldInfo> FieldInfos
+    {
+        get
+        {
+            if (fieldInfos == null) fieldInfos = new List<FieldInfo>();
+            return fieldInfos;
+        }
+    }
+
+    [MenuItem("Tools/Ability Editor")]
+    public static void GetWindow()
+    {
+        AbilityEditorWindow window = GetWindow<AbilityEditorWindow>();
+        window.position = GUIHelper.GetEditorWindowRect().AlignCenter(800, 600);
+        window.Initialise();
+    }
+
+    private void Initialise()
+    {
+        abilityTypes = Assembly.GetAssembly(typeof(PlayerAbility)).GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(PlayerAbility))).ToArray();
+        targetAbility = abilityTypes.Length == 0 ? null : abilityTypes[0];
+    }
+
+    public void Load()
+    {
+        if (targetAbility == null) return;
+
+        lastTargetAbility = targetAbility;
+
+        FieldInfos.Clear();
+
+        FieldInfos.AddRange(targetAbility.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+            .Where(f => f.GetCustomAttribute<AbilitySetting>() != null));
+
+
+        LoadAbility();
+    }
+
+    private void LoadAbility()
+    {
+         string path = PATH_PREFIX + PlayerAbilityManager.TEMPLATE_PATH + targetAbility.Name + ".json";
+
+         string text = File.ReadAllText(path);
+         targetAbilityObject = !string.IsNullOrEmpty(text) ? JsonUtility.FromJson(text, targetAbility) : Activator.CreateInstance(targetAbility);
+    }
+
+
+    [OnInspectorGUI]
+    public void OnInspectorGUI()
+    {
+        if(abilityTypes == null || targetAbility == null) Initialise();
+
+        if(FieldInfos == null || FieldInfos.Count == 0 || lastTargetAbility != targetAbility) Load();
+
+        SirenixEditorGUI.BeginBox("Defaults", true);
+
+        foreach (var field in FieldInfos)
+        {
+            if (field.FieldType == typeof(int))
+            {
+                field.SetValue(targetAbilityObject, SirenixEditorFields.IntField(field.Name, (int) field.GetValue(targetAbilityObject)));
+            }
+            else if (field.FieldType == typeof(float))
+            {
+                field.SetValue(targetAbilityObject, SirenixEditorFields.FloatField(field.Name, (float) field.GetValue(targetAbilityObject)));
+            }
+            else if (field.FieldType == typeof(bool))
+            {
+                field.SetValue(targetAbilityObject, EditorGUILayout.Toggle(field.Name, (bool) field.GetValue(targetAbilityObject)));
+            }
+            else if (field.FieldType == typeof(string))
+            {
+                field.SetValue(targetAbilityObject, SirenixEditorFields.TextField(field.Name, (string) field.GetValue(targetAbilityObject)));
+            }
+        }
+
+        SirenixEditorGUI.EndBox();
+    }
+
+
+    [Button(ButtonSizes.Large)]
+    private void SaveAbility()
+    {
+         string path = PATH_PREFIX + PlayerAbilityManager.TEMPLATE_PATH + targetAbility.Name + ".json";
+
+         string json = "{\n";
+         foreach (var info in fieldInfos)
+         {
+             json += "    " + info.Name + ": " + info.GetValue(targetAbilityObject) + ",\n";
+         }
+
+         json += "}";
+
+         File.WriteAllText(path, json);
+         AssetDatabase.Refresh();
+    }
+
+}
